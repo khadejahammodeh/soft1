3650883fb1befe960b4af39eaf025c19142b7fc5
khade <khade@khadeja_ibrahim>
2025-10-21 14:52:52 +0300

    my soft firs addition for understanding logic

---
+++ src/main/java/soft/main.java
@@ -0,0 +1,11 @@
+package soft;
+
+public class main {
+
+	public static void main(String []args)
+	{
+		inter interFrame = new inter();
+	      interFrame.setVisible(true);
+	      interFrame.setLocationRelativeTo(null);
+	}
+}
